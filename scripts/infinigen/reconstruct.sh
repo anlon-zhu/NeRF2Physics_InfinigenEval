1aa3cc7151c8221dcf48d5239a108f0622fee664
Anlon Zhu <zhu.anlon@gmail.com>
2025-03-31 01:10:35 -0400

    scene name

--- scripts/infinigen/reconstruct.sh
+++ scripts/infinigen/reconstruct.sh
@@ -12,7 +12,7 @@
 
 # Check arguments
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <data_dir> [scene_id] [training_iters] [near_plane] [far_plane]"
+    echo "Usage: $0 <data_dir> [scene_name] [training_iters] [near_plane] [far_plane]"
     echo "Example: $0 /n/fs/scratch/${USER}/nerf2physics/infinigen_nerf_data 18015bf3 10000 0.4 6.0"
     exit 1
 fi
@@ -22,7 +22,7 @@ export HF_HOME=/n/fs/scratch/${USER}/nerf2physics/hf_cache
 export TORCH_HOME=/n/fs/scratch/${USER}/nerf2physics/torch_cache
 
 DATA_DIR=$1
-SCENE_ID=$2
+SCENE_NAME=$2
 TRAINING_ITERS=${3:-10000}  # Default to 10000 if not provided
 NEAR_PLANE=${4:-0.4}        # Default to 0.4 if not provided
 FAR_PLANE=${5:-6.0}         # Default to 6.0 if not provided
@@ -38,14 +38,14 @@ cd /n/fs/pvl-progen/anlon/NeRF2Physics_InfinigenEval
 echo "[$(date)] Starting NeRF reconstruction process"
 START_TIME=$(date +%s)
 
-# If scene_id is provided, process just that scene
-if [ ! -z "$SCENE_ID" ]; then
-    echo "Reconstructing single scene: $SCENE_ID"
+# If scene_name is provided, process just that scene
+if [ ! -z "$SCENE_NAME" ]; then
+    echo "Reconstructing single scene: $SCENE_NAME"
     
     # Run NeRF Studio reconstruction
     python ns_reconstruction.py \
         --data_dir ${DATA_DIR} \
-        --scene_id ${SCENE_ID} \
+        --scene_name ${SCENE_NAME} \
         --training_iters ${TRAINING_ITERS} \
         --near_plane ${NEAR_PLANE} \
         --far_plane ${FAR_PLANE} \
