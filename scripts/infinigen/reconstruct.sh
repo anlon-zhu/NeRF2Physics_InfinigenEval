392819611fe468aa0112f09b331ee47f1ddcb921
Anlon Zhu <zhu.anlon@gmail.com>
2025-04-05 04:20:16 -0400

    partition

--- scripts/infinigen/reconstruct.sh
+++ scripts/infinigen/reconstruct.sh
@@ -3,7 +3,6 @@
 #SBATCH --output=/n/fs/scratch/%u/nerf2physics/logs/%x_%j.out
 #SBATCH --error=/n/fs/scratch/%u/nerf2physics/logs/%x_%j.err
 #SBATCH --partition=cs
-#SBATCH --account=cs
 #SBATCH --time=12:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
